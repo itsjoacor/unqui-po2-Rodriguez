54b8fe1a9239d6b96bb919a78e19087a3d697d6e
itsjoacor <jdr123@gmail.com>
2024-04-15 14:47:43 -0300

    Tp5 recien iniciado - ideas uml

---
+++ TP5/test/mercadoCentral/junitt.java
@@ -0,0 +1,14 @@
+package mercadoCentral;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class junitt {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
